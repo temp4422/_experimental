c1116430518626a4d938cfc2b288917339cd6f2e
user <test@gmail.com>
2021-09-13 14:22:18 +0300

    Add img

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp')
-const htmlmin = require('gulp-htmlmin')
-const cleanCSS = require('gulp-clean-css')
-const uglify = require('gulp-uglify')
-const brotli = require('gulp-brotli')
 const del = require('del')
+const htmlmin = require('gulp-htmlmin')
+const cleancss = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
+// const imagemin = require('gulp-imagemin')
+
+// import imagemin from 'gulp-imagemin';
+// import * as imagemin from 'imagemin';
+// import { imagemin } from 'imagemin';
+
+// const brotli = require('gulp-brotli')
 
 // Clean dir
 gulp.task('clean-dir', () => {
   return del('dist/**', { force: true })
 })
-// Gulp task to minify HTML files
+// Minify HTML files
 gulp.task('minify-html', () => {
   return gulp
     .src(['./src/**/*.html'])
@@ -25,14 +31,14 @@ gulp.task('minify-html', () => {
     )
     .pipe(gulp.dest('./dist'))
 })
-// Gulp task to minify CSS files
+// Minify CSS files
 gulp.task('minify-css', () => {
   return gulp
     .src('./src/**/*.css')
-    .pipe(cleanCSS({ level: '2' }))
+    .pipe(cleancss({ level: '2' }))
     .pipe(gulp.dest('./dist'))
 })
-// Gulp task to minify JS files
+// Minify JS files
 gulp.task('minify-js', () => {
   return gulp
     .src('./src/**/*.js')
@@ -53,10 +59,15 @@ gulp.task('minify-js', () => {
     )
     .pipe(gulp.dest('./dist'))
 })
-// Gulp task to compress with brotli algorithm
-gulp.task('compress-brotli', () => {
-  return gulp.src('./src/*').pipe(brotli.compress()).pipe(gulp.dest('./dist'))
-})
+// Compress with brotli algorithm
+// gulp.task('compress-brotli', () => {return gulp.src('./src/*').pipe(brotli.compress()).pipe(gulp.dest('./dist'))})
+
+// Compress images
+// gulp.task('compress-img', () => {
+//   return gulp.src('./src/img/**')
+//   .pipe(imagemin())
+//   .pipe(gulp.dest('./dist'))
+// })
 
 // Run all tasks with default command
-gulp.task('default', gulp.series('clean-dir', 'minify-html', 'minify-css', 'minify-js', 'compress-brotli'))
+gulp.task('default', gulp.series('clean-dir', 'minify-html', 'minify-css', 'minify-js'))
