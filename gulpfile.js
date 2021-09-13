0ec3318d793a1d187257d20aa1622405af46acfb
user <test@gmail.com>
2021-09-13 13:13:43 +0300

    Gulp add brotli compression

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,23 @@ const gulp = require('gulp')
 const htmlmin = require('gulp-htmlmin')
 const cleanCSS = require('gulp-clean-css')
 const uglify = require('gulp-uglify')
+const brotli = require('gulp-brotli')
 
 // Gulp task to minify HTML files
 gulp.task('minify-html', () => {
-  return (
-    gulp
-      .src(['./src/**/*.html'])
-      .pipe(
-        htmlmin({
-          collapseWhitespace: true,
-          removeComments: true,
-          removeOptionalTags: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeTagWhitespace: true,
-        })
-      )
-      .pipe(gulp.dest('./dist'))
-  )
+  return gulp
+    .src(['./src/**/*.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeTagWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest('./dist'))
 })
 // Gulp task to minify CSS files
 gulp.task('minify-css', () => {
@@ -49,5 +48,10 @@ gulp.task('minify-js', () => {
     )
     .pipe(gulp.dest('./dist'))
 })
-// Run all tasks with default command 
-gulp.task('default', gulp.series('minify-html', 'minify-css', 'minify-js'))
+// Gulp task to compress with brotli algorithm
+gulp.task('compress-brotli', () => {
+  return gulp.src('./src/*').pipe(brotli.compress()).pipe(gulp.dest('./dist'))
+})
+
+// Run all tasks with default command
+gulp.task('default', gulp.series('minify-html', 'minify-css', 'minify-js', 'compress-brotli'))
