4f5589fca93a381a5a52958374de7457b3132ea2
user <test@gmail.com>
2021-10-22 19:31:00 +0300

    rm gulp

--- src/script.js
+++ src/script.js
@@ -2,3 +2,10 @@
 //   // alert(this)
 //   alert('Greetings my friend!')
 // }
+
+import { hi } from './script2.js'
+export { hi }
+// hi()
+// export function hello() {
+//   console.log('hello')
+// }
