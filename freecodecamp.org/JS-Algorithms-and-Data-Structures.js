39ec95b9f3b00257ec8474fb42150d680da2a7d7
webdev4422 <it.webdev4422@gmail.com>
2021-11-23 21:33:23 +0200

    freecodecamp +

--- freecodecamp.org/JS-Algorithms-and-Data-Structures.js
+++ freecodecamp.org/JS-Algorithms-and-Data-Structures.js
@@ -923,4 +923,11 @@
 //     }
 //   }
 // }
+// function getIndexToIns(arr, num) {
+//   arr.sort((a, b) => a - b)
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= num) return i
+//   }
+//   return arr.length
+// }
 // getIndexToIns([], 1) //should return 0.
