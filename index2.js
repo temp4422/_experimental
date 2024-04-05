81f4bb0833900af7683f34d5930fc751953d204b
user <user@MacBook-Pro-user.local>
2024-04-05 11:59:04 +0300

    Refactor

--- index2.js
+++ index2.js
@@ -1,7 +1,8 @@
 function sum(a) {
   return (b) => a + b
 }
-console.log(sum(1)(2))
+// console.log(sum(1)(2))
 
 let randomNum = Math.random() > 0.5 ? 'high' : 'low'
-console.log(randomNum)
+
+console.log('OK')
