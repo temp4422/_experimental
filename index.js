14bab7812a02e631b43288f70d8855cbcad5ca3a
user <user@MacBook-Pro-user.local>
2024-05-23 17:07:20 +0300

    Experiment

--- index.js
+++ index.js
@@ -22,3 +22,75 @@
 // console.log(randomNum)
 
 // # Snippet
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++
+//   },
+//   down() {
+//     this.step--
+//   },
+//   showStep: function () {
+//     // alert(this.step)
+//     return this.step
+//   },
+// }
+// ladder.up()
+// ladder.up()
+// ladder.down()
+// ladder.showStep() //?
+
+// Chaining  https://javascript.info/object-methods#chaining
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++
+//     return this
+//   },
+//   down() {
+//     this.step--
+//     return this
+//   },
+//   showStep: function () {
+//     // alert(this.step)
+//     console.log(this.step)
+//     return this
+//   },
+// }
+// ladder.up().up().down().showStep() //?
+
+// # Snippet
+// Using "this" in object literal
+// function makeUser() {
+//   return {
+//     name: 'John',
+//     ref: this,
+//   }
+// }
+
+// let user = makeUser()
+// console.log(user)
+
+// alert(user.ref.name) // What's the result? // Error: Cannot read property 'name' of undefined
+// console.log(user.ref.name)
+
+// function makeUser() {
+//   return {
+//     name: 'John',
+//     ref() {
+//       return this
+//     },
+//   }
+// }
+
+// // let user = makeUser()
+// // console.log(user)
+
+// // alert( user.ref().name ); // John
+// // console.log(user.ref().name)
+// function sayHi() {
+//   console.log(this.name)
+// }
+// sayHi()
+
+// # Snippet
